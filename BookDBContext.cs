325c4eaeda32631e4a8fafd59e02e7f0bb9ccb2f
vinodSakpal <91941637+vinodSakpal@users.noreply.github.com>
2021-10-06 01:08:28 +0530

    Add files via upload

---
+++ BookDBContext.cs
@@ -0,0 +1,17 @@
+﻿
+using BookDetails_MicroService.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookDetails_MicroService.DBContexts
+{
+    public class BookDBContext : DbContext
+    {
+        public BookDBContext(DbContextOptions<BookDBContext> dbContext) : base(dbContext) { }
+        public DbSet<BookMaster> BookMasters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookMaster>().Property(b => b.Id).ValueGeneratedOnAdd();
+        }
+    }
+}
